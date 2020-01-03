dcde2a36197bf197a4a93e104a6174d7b7a9ffcd
jinghun1999 <minggang@outlook.com>
2020-01-03 09:08:43 +0800

    monitor build

--- js/mycharts.js
+++ js/mycharts.js
@@ -1,4 +1,9 @@
-﻿if ($(window).width() > 9910) {
+﻿/* 
+AUTH: minggang@outlook.com
+TIME: 2019/12/28
+USAGE: INDIA MGI FACTORY LES LOG
+*/
+if ($(window).width() > 9910) {
     var total = window.innerHeight;
     document.getElementById("title").style.height = total * 0.1 + "px";
     document.getElementById("main").style.height = total * 0.9 + "px";
@@ -26,28 +31,113 @@
     //document.getElementById("ym-menu").style.height = box03_h * 0.1 + "px"
 };
 var app = angular.module('myApp', []);
-app.controller('customersCtrl',
-function($scope, $http) {
-    $http({
-        method: 'get',
-        url: 'js/da.json'
-    }).then(function(res) {
-        $scope.parts = res.data.parts;
-        $scope.sheets = res.data.sheets;
-        $scope.daily_report = res.data.daily_report;
-        $scope.listData1 = res.data.listData1;
-        var worldMapContainer2 = document.getElementById('box2');
-        //var box01 = document.getElementById("box01");
-        //var box01_h = box01.offsetHeight;
-        //var box01_w = box01.offsetWidth;
-        //debugger
-        var resizeWorldMapContainer2 = function() {
-            worldMapContainer2.style.width = box01_w * 0.96 + 'px';
-            worldMapContainer2.style.height = box01_h * 0.38 + 'px'
-        };
-        //resizeWorldMapContainer2();
-        var myChart = echarts.init(worldMapContainer2);
-        var option = {
+app.controller('customersCtrl', function($scope, $http, $interval) {
+    $scope.pager = {
+        parts: {ps: 7, pi: 1, pc:1},
+        sheets: {ps: 7, pi: 1, pc:1},
+        overflow: {ps: 12, pi: 1, pc:1},
+        lack: {ps: 12, pi: 1, pc:1},
+    };
+    var getData = function(){
+        $http({
+            method: 'post',
+            //url: 'js/da.json',
+            url: 'http://localhost:49280/api/dashboard/getdb',
+            data: $scope.pager
+        }).then(function(res) {
+            debugger;
+            if(res.data.successful){
+                $scope.parts = res.data.data.parts;
+                $scope.sheets = res.data.data.sheets;
+                $scope.daily_report = res.data.data.daily_report;
+                $scope.pager = res.data.data.pager;
+                var worldMapContainer2 = document.getElementById('box2');
+                //var box01 = document.getElementById("box01");
+                //var box01_h = box01.offsetHeight;
+                //var box01_w = box01.offsetWidth;
+                //debugger
+                var resizeWorldMapContainer2 = function() {
+                    //worldMapContainer2.style.width = box01_w * 0.96 + 'px';
+                    //worldMapContainer2.style.height = box01_h * 0.38 + 'px'
+                };
+                //resizeWorldMapContainer2();
+                var myChart = echarts.init(worldMapContainer2);
+                option = getRiskOverflowOption(res.data.data.risk_overflow);
+                myChart.setOption(option);
+                window.onresize = function() {
+                    resizeWorldMapContainer2();
+                    myChart.resize()
+                };
+                myChart = echarts.init(document.getElementById('box4'));
+                myChart.setOption(option);
+                var worldMapContainer = document.getElementById('box3');
+                box03 = document.getElementById("box03");
+                box03_h = box03.offsetHeight;
+                box03_w = box04.offsetWidth;
+                var resizeWorldMapContainer = function() {
+                    //worldMapContainer.style.width = box03_w * 0.8 + 'px';
+                    //worldMapContainer.style.height = box03_h * 1.1 + 'px'
+
+                    //worldMapContainer.style.width = box03_w+'px';
+                };
+                resizeWorldMapContainer();
+                var myChart = echarts.init(worldMapContainer);
+                let _kd_titles = [];
+                for(let i = 0; i< res.data.data.kd_list.length; i++){
+                    _kd_titles.push(res.data.data.kd_list[i].name);
+                }
+                var option = {
+                    tooltip: {
+                        trigger: 'item',
+                        formatter: "{a} <br/>{b}: {c} ({d}%)"
+                    },
+                    legend: {
+                        x: 'center',
+                        y: 'bottom',
+                        textStyle:{ color:'#ccc'},
+                        data: _kd_titles
+                    },
+                    series: [
+                        {
+                            name:'KD',
+                            type:'pie',
+                            radius: ['40%', '55%'],
+                            label: {
+                                normal: {
+                                    formatter: '{b|{b}}\n{c}',
+                                    rich: {
+                                        b: {
+                                            fontSize: 12,
+                                            lineHeight: 12
+                                        }
+                                    }
+                                }
+                            },
+                            data: res.data.data.kd_list
+                        }
+                    ]
+                };
+                myChart.setOption(option);
+                window.onresize = function() {
+                    resizeWorldMapContainer();
+                    myChart.resize()
+                };
+            }else{
+                console.error(res.data.data);
+            }
+        });
+    }
+
+    var getRiskOverflowOption = function(d){
+        let titles = [];
+        let now_and_way = [];
+        let max_storage = [];
+        for(var i = 0; i<d.length; i++){
+            titles.push(d[i].pno);
+            now_and_way.push(Math.random()*d[i].now_way);
+            max_storage.push(Math.random()*d[i].max);
+        }
+        var op = {
             //color: ['#38b3f1'],
             tooltip: {
                 trigger: 'axis',
@@ -79,77 +169,30 @@ function($scope, $http) {
             }],
             yAxis: [{
                 type: 'category',
-                data: res.data.titleList,
+                data: titles,
             }],
             series: [{
                 name: '当前库存+在途库存',
                 type: 'bar',
                 color:'#22DD92',
                 barWidth: '20%',
-                data: res.data.now_and_way
+                data: now_and_way
             },
             {
                 name: '最大库存',
                 type: 'bar',
                 color:'#EEC591',
                 barWidth: '10%',
-                data: res.data.max_storage
+                data: max_storage
             }]
         };
-        myChart.setOption(option);
-        window.onresize = function() {
-            resizeWorldMapContainer2();
-            myChart.resize()
-        };
-        myChart = echarts.init(document.getElementById('box4'));
-        myChart.setOption(option);
-        var worldMapContainer = document.getElementById('box3');
-        box03 = document.getElementById("box03");
-        box03_h = box03.offsetHeight;
-        box03_w = box04.offsetWidth;
-        var resizeWorldMapContainer = function() {
-            //worldMapContainer.style.width = box03_w * 0.8 + 'px';
-            worldMapContainer.style.height = box03_h * 1.1 + 'px'
 
-            //worldMapContainer.style.width = box03_w+'px';
-        };
-        resizeWorldMapContainer();
-        var myChart = echarts.init(worldMapContainer);
-        var option = {
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b}: {c} ({d}%)"
-            },
-            legend: {
-                x: 'center',
-                y: 'bottom',
-                textStyle:{ color:'#ccc'},
-                data: res.data.kd_titles
-            },
-            series: [
-                {
-                    name:'KD',
-                    type:'pie',
-                    radius: ['40%', '55%'],
-                    label: {
-                        normal: {
-                            formatter: '{b|{b}}\n{c}',
-                            rich: {
-                                b: {
-                                    fontSize: 12,
-                                    lineHeight: 12
-                                }
-                            }
-                        }
-                    },
-                    data: res.data.kd_list
-                }
-            ]
-        };
-        myChart.setOption(option);
-        window.onresize = function() {
-            resizeWorldMapContainer();
-            myChart.resize()
-        };
-    });
+        return op;
+    }
+
+    // 先加载一次
+    getData();
+
+    // 定时刷新页面
+    $interval(function(){getData()}, 1000 * 30, -1);
 });
