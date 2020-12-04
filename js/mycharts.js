4447b28a2b43013a21676a9bb165478dd3c96bc0
jinghun1999 <minggang@outlook.com>
2020-12-04 10:08:32 +0800

    大屏幕发布

--- js/mycharts.js
+++ js/mycharts.js
@@ -9,14 +9,14 @@ if ($(window).width() > 9910) {
     document.getElementById("main").style.height = total * 0.9 + "px";
     title = document.getElementById("title");
     main = document.getElementById("main");
-    box02 = document.getElementById("box02");
-    box03 = document.getElementById("box03");
-    box04 = document.getElementById("box04");
+    //box02 = document.getElementById("box02");
+    box03 = document.getElementById("manul_1");
+    //box04 = document.getElementById("box04");
     title_h = title.offsetHeight;
     main_h = main.offsetHeight;
-    box02_h = box02.offsetHeight;
-    box03_h = box03.offsetHeight;
-    box04_h = box04.offsetHeight;
+    //box02_h = box02.offsetHeight;
+    //box03_h = box03.offsetHeight;
+    //box04_h = box04.offsetHeight;
     document.getElementById("box01").style.height = main_h * 0.65 + "px";
     document.getElementById("box8-box").style.height = main_h * 0.3 + "px";
     box02.style.height = main_h * 0.62 + "px";
@@ -38,168 +38,121 @@ app.controller('customersCtrl', function($scope, $http, $interval) {
         overflow: {ps: 10, pi: 1, pc:1},
         lack: {ps: 10, pi: 1, pc:1},
     };
-    var getData = function(){
+    var timer1, timer2, timer3;
+    var getChart = function() {
         $http({
-            method: 'post',
-            //url: 'js/da.json',
-            url: 'http://localhost:49280/api/dashboard/getdb',
-            data: $scope.pager
+            method: 'get',
+            url: API_HOST + 'api/dashboard/getCustChart'
         }).then(function(res) {
             if(res.data.successful){
-                let dt = res.data.data;
-
-                $scope.parts = dt.parts;
-                $scope.sheets = dt.sheets;
-                $scope.daily_report = dt.daily_report;
-                $scope.pager = dt.pager;
-                var worldMapContainer2 = document.getElementById('box2');
-                //var box01 = document.getElementById("box01");
-                //var box01_h = box01.offsetHeight;
-                //var box01_w = box01.offsetWidth;
-                //debugger
-                var resizeWorldMapContainer2 = function() {
-                    //worldMapContainer2.style.width = box01_w * 0.96 + 'px';
-                    //worldMapContainer2.style.height = box01_h * 0.38 + 'px'
-                };
-                //resizeWorldMapContainer2();
-                var myChart = echarts.init(worldMapContainer2);
-                // overflow
-                option = getIssueOption(dt.risk_overflow, 'Max');
-                myChart.setOption(option);
-                window.onresize = function() {
-                    resizeWorldMapContainer2();
-                    myChart.resize()
-                };
-
-                // lack
-                option = getIssueOption(dt.risk_lack, 'Min');
-                myChart = echarts.init(document.getElementById('box4'));
-                myChart.setOption(option);
-
-
-                var worldMapContainer = document.getElementById('box3');
-                box03 = document.getElementById("box03");
-                box03_h = box03.offsetHeight;
-                box03_w = box04.offsetWidth;
-                var resizeWorldMapContainer = function() {
-                    //worldMapContainer.style.width = box03_w * 0.8 + 'px';
-                    //worldMapContainer.style.height = box03_h * 1.1 + 'px'
-
-                    //worldMapContainer.style.width = box03_w+'px';
-                };
-                resizeWorldMapContainer();
-                var myChart = echarts.init(worldMapContainer);
-                let _kd_titles = [];
-                for(let i = 0; i< dt.kd_list.length; i++){
-                    _kd_titles.push(dt.kd_list[i].name);
-                }
-                var option = {
-                    tooltip: {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b}: {c} ({d}%)"
-                    },
-                    legend: {
-                        x: 'center',
-                        y: 'bottom',
-                        textStyle:{ color:'#ccc'},
-                        data: _kd_titles
-                    },
-                    series: [
-                        {
-                            name:'KD',
-                            type:'pie',
-                            radius: ['40%', '55%'],
-                            label: {
-                                normal: {
-                                    formatter: '{b|{b}}\n{c}',
-                                    rich: {
-                                        b: {
-                                            fontSize: 12,
-                                            lineHeight: 12
-                                        }
-                                    }
-                                }
-                            },
-                            data: dt.kd_list
-                        }
-                    ]
-                };
-                myChart.setOption(option);
-                window.onresize = function() {
-                    resizeWorldMapContainer();
-                    myChart.resize()
-                };
+                var custs = res.data.data;
+                renderChart('manul', custs.manul, timer1);
+                renderChart('smpv', custs.smpv, timer2);
+                renderChart('smcv', custs.smcv, timer3);
             }else{
                 console.error(res.data);
             }
         });
     }
-
-    var getIssueOption = function(d, t){
-        let titles = [];
-        let now_and_way = [];
-        let max_storage = [];
-        for(var i = 0; i<d.length; i++){
-            titles.push(d[i].pno);
-            now_and_way.push(d[i].now_way);
-            max_storage.push(d[i].max);
+    var renderChart = function(cid, dt, timer) {        
+        var dom = document.getElementById('chart_' + cid);
+        var myChart = echarts.init(dom);
+        let i = 0;
+        if(timer) {
+            clearInterval(timer);
         }
-        var op = {
-            //color: ['#38b3f1'],
+        timer = $interval(function(){
+            switch(cid) {
+                case 'manul':
+                    $scope.manul_no = dt[i].cust_order_no;
+                    $scope.manul_length = dt.length;
+                    $scope.manul_cur = i + 1;
+                    break;
+                case 'smpv':
+                    $scope.smpv_no = dt[i].cust_order_no;
+                    $scope.smpv_length = dt.length;
+                    $scope.smpv_cur = i + 1;
+                    break;
+                case 'smcv':
+                    $scope.smcv_no = dt[i].cust_order_no;
+                    $scope.smcv_length = dt.length;
+                    $scope.smcv_cur = i + 1;
+                    break;
+            }            
+            option = getChartOption(dt[i]);
+            myChart.setOption(option);
+            if(i < dt.length - 1){
+                i++;
+            } else {
+                i = 0;
+            }
+        }, 1000 * 20, -1);
+        window.onresize = function() {
+            myChart.resize()
+        };
+    }
+    var getData = function(){
+        $http({
+            method: 'post',
+            //url: 'js/da.json',
+            url: API_HOST + 'api/dashboard/getdb',
+            data: $scope.pager
+        }).then(function(res) {
+            if(res.data.successful){
+                let dt = res.data.data;
+                $scope.sheets = dt.sheets;
+                $scope.daily_report = dt.daily_report;
+                $scope.sap_summary = dt.sap_summary;
+                $scope.pager = dt.pager;
+            }else{
+                console.error(res.data);
+            }
+        });
+    }
+    var getChartOption = function(d) {
+        var option = {
             tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow'
-                }
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
             },
             legend: {
-                //bottom:0,
-                textStyle:{color:'#ccc'},
-                //padding:50,
-                data: ['Current+Way', t+' Stock']
+                x: 'center',
+                y: 'bottom',
+                textStyle: { color:'#ccc'},
+                data: ['Receipt Qty', 'Transit Qty', 'Pending Qty']
             },
-            textStyle: {
-                color: '#ccc'
-            },
-            grid: {
-                top: '10%',
-                left: '3%',
-                right: '3%',
-                bottom: '6%',
-                containLabel: true
-            },
-            xAxis: [{
-                type: 'value',
-                axisTick: {
-                    alignWithLabel: true
+            series: [
+                {
+                    name:'KD',
+                    type:'pie',
+                    radius: ['40%', '55%'],
+                    label: {
+                        normal: {
+                            formatter: '{b|{b}}\n{c}',
+                            rich: {
+                                b: {
+                                    fontSize: 12,
+                                    lineHeight: 12
+                                }
+                            }
+                        }
+                    },
+                    data: [
+                        { name: 'Receipt Qty', value: d.receipt }, 
+                        { name: 'Transit Qty', value: d.transit }, 
+                        { name: 'Pending Qty', value: d.pending }
+                    ]
                 }
-            }],
-            yAxis: [{
-                type: 'category',
-                data: titles,
-            }],
-            series: [{
-                name: 'Current+Way',
-                type: 'bar',
-                color:'#22DD92',
-                barWidth: '20%',
-                data: now_and_way
-            },
-            {
-                name: t+' Stock',
-                type: 'bar',
-                color:'#f4e925',
-                barWidth: '10%',
-                data: max_storage
-            }]
+            ]
         };
-
-        return op;
+        return option;
     }
-
     // 先加载一次
+    getChart();
     getData();
 
     // 定时刷新页面
-    $interval(function(){getData()}, 1000 * 30, -1);
+    $interval(function(){
+        getData()
+    }, 1000 * 120, -1);
 });
